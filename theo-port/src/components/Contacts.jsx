6b667198d1af83e667a2bfc0be5bffa8d430f9b2
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-04-22 23:34:07 +0100

    Email can now be sent

--- theo-port/src/components/Contacts.jsx
+++ theo-port/src/components/Contacts.jsx
@@ -2,20 +2,40 @@ import React from "react";
 import '../Contacts.css';
 
 function Contacts(){
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            message: e.target.message.value,
+        };
+
+
+        try {
+            const response = await fetch("http://localhost:5000/send-email", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const result = await response.json();
+            alert(result.message);
+            e.target.reset();
+        } catch (err) {
+            alert("Failed to send message. Please try again!");
+        }
+    };
     return(
         <>
         <section id="contact" className="contacts">
             <h2>Get in Touch</h2>
-            <form action="https://formspree.io/f/xvgaogba" method="POST">
-                <label>
-                    <span>Your email:</span>
-                    <input type="email" name="email"/>
-                </label>
-                <label>
-                <span>Your message:</span>
-                    <textarea name="message"></textarea>
-                </label>
-                    <button type="submit">Send</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="Name" required />
+                <input type="email" name="email" placeholder="Email" required />
+                <textarea name="message" placeholder="Message" required></textarea>
+                <button type="submit">Send</button>
             </form>
         </section>
         </>
