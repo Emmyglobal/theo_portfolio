9037f604be0f121dd5178adaeb62c11e52705e6f
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-04-24 14:58:59 +0100

    corrections made

--- contact-backend/index.js
+++ contact-backend/index.js
@@ -7,11 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "https://theo-portfolio-i.vercel.app", // Your deployed frontend
+    origin: "https://theo-portfolio-u.vercel.app/", // Your deployed frontend
     methods: ["POST"],
     credentials: true,
   }));
-    
+
 app.use(express.json());
 
 app.listen(PORT, () => {
